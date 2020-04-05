409326f8bebafb1c4b8c55afd2be54419865a71c
Гвоздков Михаил Александрович <mgvozdko@astronis.ru>
2020-04-06 00:11:26 +0300

    author replaceb by committer

--- src/js/components/commitcard.js
+++ src/js/components/commitcard.js
@@ -10,8 +10,8 @@ export class CommitCard {
     create(commit) {
       const card = this.cardTemplate.content.cloneNode(true);
       card.querySelector(".commit__link").setAttribute("href", commit.html_url);
-      card.querySelector(".commit__date").setAttribute("datetime", commit.commit.author.date);
-      card.querySelector(".commit__date").textContent = formatDateAsLongString(new Date(commit.commit.author.date));
+      card.querySelector(".commit__date").setAttribute("datetime", commit.commit.committer.date);
+      card.querySelector(".commit__date").textContent = formatDateAsLongString(new Date(commit.commit.committer.date));
 
       if (commit.author !== null && commit.author.avatar_url !== null) {
           this._imageNotFoundHandler = this._imageNotFound.bind(this);
@@ -21,10 +21,10 @@ export class CommitCard {
           this._imageLoadedHandler = this._imageLoaded.bind(this);
           card.querySelector(".commit__avatar").addEventListener("load", this._imageLoadedHandler);
       }
-      card.querySelector(".commit__avatar").setAttribute("alt", commit.commit.author.name);
-      card.querySelector(".commit__author-name").textContent = commit.commit.author.name;
+      card.querySelector(".commit__avatar").setAttribute("alt", commit.commit.committer.name);
+      card.querySelector(".commit__author-name").textContent = commit.commit.committer.name;
       //card.querySelector(".commit__author-email").setAttribute("href", `mailto:${commit.commit.author.email}`);
-      card.querySelector(".commit__author-email").textContent = commit.commit.author.email;
+      card.querySelector(".commit__author-email").textContent = commit.commit.committer.email;
       card.querySelector(".commit__description").textContent = commit.commit.message;
       return card;
     }
