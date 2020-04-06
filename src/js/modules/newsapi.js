ce09353cf6eeb108ad4473b9b98b5cb850a68018
Гвоздков Михаил Александрович <mgvozdko@astronis.ru>
2020-04-06 03:11:37 +0300

    error messages constants arrary moved from function

--- src/js/modules/newsapi.js
+++ src/js/modules/newsapi.js
@@ -1,6 +1,27 @@
 //import { getMesaageByHTMLStatus } from "./../utils/html-status";
 import { NETWORK_ERROR_MESSAGE, UNKNOWN_ERROR_MESSAGE } from "./../constants/constatns";
 
+const apiMessages = {
+    "apiKeyDisabled": "Your API key has been disabled.",
+    "apiKeyExhausted": "Your API key has no more requests available",
+    "apiKeyInvalid": "Your API key hasn't been entered correctly. Double check it and try again",
+    "apiKeyMissing": "Your API key is missing from the request. Append it to the request with one of these methods",
+    "parameterInvalid": "You've included a parameter in your request which is currently not supported. Check the message property for more details",
+    "parametersMissing": "Required parameters are missing from the request and it cannot be completed. Check the message property for more details",
+    "rateLimited": "You have been rate limited. Back off for a while before trying the request again",
+    "sourcesTooMany": "You have requested too many sources in a single request. Try splitting the request into 2 smaller requests",
+    "sourceDoesNotExist": "You have requested a source which does not exist",
+    "unexpectedError": "This shouldn't happen, and if it does then it's our fault, not yours. Try the request again shortly"
+};
+
+const htmlMessages = {
+    200: "OK",
+    400: "Bad Request. The request was unacceptable, often due to a missing or misconfigured parameter",
+    401: "Unauthorized. Your API key was missing from the request, or wasn't correct",
+    429: "Too Many Requests. You made too many requests within a window of time and have been rate limited. Back off for a while",
+    500: "Server Error. Something went wrong on our side"
+};
+
 export class NewsAPI {
 
     constructor(apiKey) {
@@ -44,32 +65,11 @@ export class NewsAPI {
     }
 
     _getMessageByStatus(result) {
-        const messages = {
-            "apiKeyDisabled": "Your API key has been disabled.",
-            "apiKeyExhausted": "Your API key has no more requests available",
-            "apiKeyInvalid": "Your API key hasn't been entered correctly. Double check it and try again",
-            "apiKeyMissing": "Your API key is missing from the request. Append it to the request with one of these methods",
-            "parameterInvalid": "You've included a parameter in your request which is currently not supported. Check the message property for more details",
-            "parametersMissing": "Required parameters are missing from the request and it cannot be completed. Check the message property for more details",
-            "rateLimited": "You have been rate limited. Back off for a while before trying the request again",
-            "sourcesTooMany": "You have requested too many sources in a single request. Try splitting the request into 2 smaller requests",
-            "sourceDoesNotExist": "You have requested a source which does not exist",
-            "unexpectedError": "This shouldn't happen, and if it does then it's our fault, not yours. Try the request again shortly"
-        }
-        
-        return messages[result.code] || UNKNOWN_ERROR_MESSAGE;
+        return apiMessages[result.code] || UNKNOWN_ERROR_MESSAGE;
     }
 
     _getMesaageByHTMLCode(code) {
-        const messages = {
-            200: "OK",
-            400: "Bad Request. The request was unacceptable, often due to a missing or misconfigured parameter",
-            401: "Unauthorized. Your API key was missing from the request, or wasn't correct",
-            429: "Too Many Requests. You made too many requests within a window of time and have been rate limited. Back off for a while",
-            500: "Server Error. Something went wrong on our side"
-        };
-
-        return messages[code] || UNKNOWN_ERROR_MESSAGE;
+        return htmlMessages[code] || UNKNOWN_ERROR_MESSAGE;
     }
 
 /*
